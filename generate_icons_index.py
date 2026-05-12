65ae6aea867f325f6c527e08334beaa3020f7b94
mapyr <mapyr@users.noreply.github.com>
2026-05-12 03:08:25 +0200

    chore: remove python utility scripts and temporary tsx icon files

--- generate_icons_index.py
+++
@@ -1,7 +0,0 @@
-import os
-
-icons = [f[:-4] for f in os.listdir('packages/site/src/components/icons') if f.endswith('.tsx')]
-with open('packages/site/src/components/Icons.tsx', 'w') as f:
-    for icon in icons:
-        f.write(f'export * from "./icons/{icon}";\n')
-
--- packages/site/src/components/icons/CubeIcon.tsx
+++
@@ -1,6 +0,0 @@
-export const CubeIcon = () => (
-    <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"/>
-    </svg>
-);
-
